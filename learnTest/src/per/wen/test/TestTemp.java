6865196108e75eea611301972f92fcc705943961
wangheng54088 <872862305@qq.com>
2020-11-21 23:37:16 +0900

    new

---
+++ learnTest/src/per/wen/test/TestTemp.java
@@ -0,0 +1,15 @@
+package per.wen.test;
+
+import java.io.FileWriter;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class TestTemp {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("file.encoding"));
+	}
+}
